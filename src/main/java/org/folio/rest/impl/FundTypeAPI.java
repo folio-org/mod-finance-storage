fb384bbcbc4e7109560887757a848f91c190bbc3
Aliaksandr_Pautau <aliaksandr_pautau@epam.com>
2019-08-29 10:56:40 +0300

    MODFISTO-19 Update fund schema

--- src/main/java/org/folio/rest/impl/FundTypeAPI.java
+++ src/main/java/org/folio/rest/impl/FundTypeAPI.java
@@ -3,6 +3,7 @@ package org.folio.rest.impl;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Handler;
+import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.model.FundType;
 import org.folio.rest.jaxrs.model.FundTypeCollection;
 import org.folio.rest.jaxrs.resource.FinanceStorageFundTypes;
@@ -22,21 +23,25 @@ public class FundTypeAPI implements FinanceStorageFundTypes {
   }
 
   @Override
+  @Validate
   public void postFinanceStorageFundTypes(String lang, FundType entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     PgUtil.post(FUND_TYPE_TABLE, entity, okapiHeaders, vertxContext, PostFinanceStorageFundTypesResponse.class, asyncResultHandler);
   }
 
   @Override
+  @Validate
   public void getFinanceStorageFundTypesById(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     PgUtil.getById(FUND_TYPE_TABLE, FundType.class, id, okapiHeaders, vertxContext, GetFinanceStorageFundTypesByIdResponse.class, asyncResultHandler);
   }
 
   @Override
+  @Validate
   public void deleteFinanceStorageFundTypesById(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     PgUtil.deleteById(FUND_TYPE_TABLE, id, okapiHeaders, vertxContext, DeleteFinanceStorageFundTypesByIdResponse.class, asyncResultHandler);
   }
 
   @Override
+  @Validate
   public void putFinanceStorageFundTypesById(String id, String lang, FundType entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     PgUtil.put(FUND_TYPE_TABLE, entity, id, okapiHeaders, vertxContext, PutFinanceStorageFundTypesByIdResponse.class, asyncResultHandler);
   }
