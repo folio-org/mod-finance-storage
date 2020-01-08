56d4cfe982d7104528f4211951dedb7cad421bbd
Siarhei_Hrabko <siarhei_hrabko@epam.com>
2020-01-08 15:45:36 +0300

    MODFISTO-63 RMB updated

--- src/test/java/org/folio/rest/utils/TestEntities.java
+++ src/test/java/org/folio/rest/utils/TestEntities.java
@@ -22,7 +22,7 @@ import org.folio.rest.persist.HelperUtils;
 
 public enum TestEntities {
   //The Order is important because of the foreign key relationships
-  FISCAL_YEAR(HelperUtils.getEndpoint(FinanceStorageFiscalYears.class), FiscalYear.class, "data/fiscal-years/", "fy20.json", "name", "FY19", 3),
+  FISCAL_YEAR(HelperUtils.getEndpoint(FinanceStorageFiscalYears.class), FiscalYear.class, "data/fiscal-years/", "fy20.json", "name", "FY20", 3),
   LEDGER(HelperUtils.getEndpoint(FinanceStorageLedgers.class), Ledger.class, "data/ledgers/", "One-time.json", "code", "One-time", 3),
   FUND_TYPE(HelperUtils.getEndpoint(FinanceStorageFundTypes.class), FundType.class, "data/fund-types/", "approvals.json", "name", "New type name", 26),
   FUND(HelperUtils.getEndpoint(FinanceStorageFunds.class), Fund.class, "data/funds/", "AFRICAHIST.json", "name", "African History", 21),
