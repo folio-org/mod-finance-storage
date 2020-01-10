c107bd9aa8ab06072f46870675f6996795569736
Siarhei_Hrabko <siarhei_hrabko@epam.com>
2020-01-10 11:30:02 +0300

    MODFISTO-63 merge conflicts

--- src/main/java/org/folio/rest/transaction/AllOrNothingHandler.java
+++ src/main/java/org/folio/rest/transaction/AllOrNothingHandler.java
@@ -9,29 +9,21 @@ import static org.folio.rest.persist.HelperUtils.rollbackTransaction;
 import static org.folio.rest.persist.HelperUtils.startTx;
 import static org.folio.rest.persist.PostgresClient.pojo2json;
 
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Context;
-import io.vertx.core.Future;
-import io.vertx.core.Handler;
-import io.vertx.core.Promise;
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.handler.impl.HttpStatusException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
 import javax.ws.rs.core.Response;
-import org.folio.rest.jaxrs.model.Budget;
-import org.folio.rest.jaxrs.model.Error;
 
 import org.folio.rest.jaxrs.model.Budget;
+import org.folio.rest.jaxrs.model.Error;
 import org.folio.rest.jaxrs.model.Errors;
-import org.folio.rest.jaxrs.model.Parameter;
 import org.folio.rest.jaxrs.model.Ledger;
 import org.folio.rest.jaxrs.model.LedgerCollection;
+import org.folio.rest.jaxrs.model.Parameter;
 import org.folio.rest.jaxrs.model.Transaction;
 import org.folio.rest.jaxrs.resource.FinanceStorageLedgers;
 import org.folio.rest.jaxrs.resource.FinanceStorageTransactions;
@@ -41,6 +33,15 @@ import org.folio.rest.persist.Tx;
 import org.folio.rest.persist.Criteria.Criteria;
 import org.folio.rest.persist.Criteria.Criterion;
 import org.javamoney.moneta.Money;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.handler.impl.HttpStatusException;
 
 public abstract class AllOrNothingHandler extends AbstractTransactionHandler {
 
