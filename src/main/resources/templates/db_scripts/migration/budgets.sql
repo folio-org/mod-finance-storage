dab29f60e4594daa359d1ed6c9aed5af210127d9
Aliaksandr_Pautau <aliaksandr_pautau@epam.com>
2020-02-25 17:05:13 +0300

    MODFISTO-83 Migration script improvement to support transition of non-unique index to unique one
    - move creation of unique index on budget.name from schema.json to migration/budget.sql

--- src/main/resources/templates/db_scripts/migration/budgets.sql
+++ src/main/resources/templates/db_scripts/migration/budgets.sql
@@ -56,4 +56,4 @@ UPDATE ${myuniversity}_${mymodule}.budget
 SET jsonb = jsonb || jsonb_build_object('name', jsonb ->> 'name' || '_' || floor(random() * 2147483646 + 1)::int)
 WHERE jsonb ->> 'name' IN (SELECT jsonb->>'name' AS name FROM ${myuniversity}_${mymodule}.budget GROUP BY name HAVING COUNT(*) > 1);
 
-
+CREATE UNIQUE INDEX IF NOT EXISTS budget_name_idx_unique ON ${myuniversity}_${mymodule}.budget (lower(f_unaccent(jsonb->>'name')));
