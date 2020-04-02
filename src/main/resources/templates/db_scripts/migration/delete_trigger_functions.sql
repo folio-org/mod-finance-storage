94e282d75a806bb13d333fa22a16f24b7a472e02
Aliaksandr_Pautau <aliaksandr_pautau@epam.com>
2020-04-02 13:41:00 +0300

    MODFISTO-97 Issues upgrading module from Edelweiss to Fameflower
    - adding migration script to delete trigger functions after table deletion

---
+++ src/main/resources/templates/db_scripts/migration/delete_trigger_functions.sql
@@ -0,0 +1,3 @@
+DROP FUNCTION IF EXISTS ${myuniversity}_${mymodule}.fund_distribution_set_md();
+DROP FUNCTION IF EXISTS ${myuniversity}_${mymodule}.set_fund_distribution_md_json();
+DROP FUNCTION IF EXISTS ${myuniversity}_${mymodule}.update_fund_distribution_references();
