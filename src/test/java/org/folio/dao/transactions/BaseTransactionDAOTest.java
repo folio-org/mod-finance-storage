853f24b0e10fbfe94af5923e0ebdd8f619fe95a5
Aliaksandr_Pautau <aliaksandr_pautau@epam.com>
2020-06-11 14:17:02 +0300

    MODFISTO-110 mod-finance-storage: Update to RMB v30.0.2
    - uncomment tests

--- src/test/java/org/folio/dao/transactions/BaseTransactionDAOTest.java
+++ src/test/java/org/folio/dao/transactions/BaseTransactionDAOTest.java
@@ -14,19 +14,15 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.UUID;
 
-import io.vertx.core.json.JsonArray;
-import io.vertx.sqlclient.Tuple;
 import org.folio.rest.jaxrs.model.Transaction;
 import org.folio.rest.persist.DBClient;
 import org.folio.rest.persist.PostgresClient;
-import org.folio.rest.persist.Criteria.Criterion;
 import org.folio.rest.persist.SQLConnection;
+import org.folio.rest.persist.Criteria.Criterion;
 import org.folio.rest.persist.interfaces.Results;
-import org.glassfish.jersey.internal.Errors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -42,6 +38,10 @@ import io.vertx.core.Vertx;
 import io.vertx.ext.web.handler.impl.HttpStatusException;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+import io.vertx.pgclient.PgException;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.Tuple;
 
 @ExtendWith(VertxExtension.class)
 class BaseTransactionDAOTest {
@@ -62,30 +62,27 @@ class BaseTransactionDAOTest {
       BaseTransactionDAO.class, Mockito.CALLS_REAL_METHODS);
   }
 
-//  @Test
-//  void getTransactionsWithGenericDatabaseException(Vertx vertx, VertxTestContext testContext) {
-//    when(client.getPgClient()).thenReturn(postgresClient);
-//
-//    doAnswer((Answer<Void>) invocation -> {
-//      Handler<AsyncResult<Results<Transaction>>> handler = invocation.getArgument(5);
-//      Map<Character, String> fields = new HashMap<>();
-//      fields.put('C', "22P02");
-//      fields.put('M', "Test");
-//      handler.handle(Future.failedFuture(new Exception(new Errors.ErrorMessage(fields))));
-//      return null;
-//    }).when(postgresClient).get(eq(TRANSACTIONS_TABLE), eq(Transaction.class), any(Criterion.class), anyBoolean(), anyBoolean(), any(Handler.class));
-//
-//
-//    testContext.assertFailure(baseTransactionDAO.getTransactions(new Criterion(), client))
-//      .onComplete(event -> {
-//        HttpStatusException exception = (HttpStatusException) event.cause();
-//        testContext.verify(() -> {
-//          assertEquals(exception.getStatusCode() , 400);
-//          assertEquals(exception.getPayload(), "Test");
-//        });
-//        testContext.completeNow();
-//      });
-//  }
+  @Test
+  void getTransactionsWithGenericDatabaseException(Vertx vertx, VertxTestContext testContext) {
+    when(client.getPgClient()).thenReturn(postgresClient);
+
+    doAnswer((Answer<Void>) invocation -> {
+      Handler<AsyncResult<Results<Transaction>>> handler = invocation.getArgument(5);
+      handler.handle(Future.failedFuture(new PgException("Test", "Test", "22P02", "Test")));
+      return null;
+    }).when(postgresClient).get(eq(TRANSACTIONS_TABLE), eq(Transaction.class), any(Criterion.class), anyBoolean(), anyBoolean(), any(Handler.class));
+
+
+    testContext.assertFailure(baseTransactionDAO.getTransactions(new Criterion(), client))
+      .onComplete(event -> {
+        HttpStatusException exception = (HttpStatusException) event.cause();
+        testContext.verify(() -> {
+          assertEquals(exception.getStatusCode() , 400);
+          assertEquals(exception.getPayload(), "Test");
+        });
+        testContext.completeNow();
+      });
+  }
 
   @Test
   void getTransactionsWithConnection(Vertx vertx, VertxTestContext testContext) {
@@ -111,35 +108,32 @@ class BaseTransactionDAOTest {
       });
   }
 
-//  @Test
-//  void saveTransactionsToPermanentTable(Vertx vertx, VertxTestContext testContext) {
-//    when(client.getPgClient()).thenReturn(postgresClient);
-//    when(client.getConnection()).thenReturn(connection);
-//    when(client.getTenantId()).thenReturn("test");
-//
-//    Mockito.when(baseTransactionDAO.createPermanentTransactionsQuery(anyString()))
-//      .thenReturn("test.table");
-//
-//    doAnswer((Answer<Void>) invocation -> {
-//      Handler<AsyncResult<UpdateResult>> handler = invocation.getArgument(3);
-//      Map<Character, String> fields = new HashMap<>();
-//      fields.put('C', "22P02");
-//      fields.put('M', "Test");
-//      handler.handle(Future.failedFuture(new GenericDatabaseException(new ErrorMessage(fields))));
-//      return null;
-//    }).when(postgresClient).execute(eq(connection), eq("test.table"), any(Tuple.class), any(Handler.class));
-//
-//
-//    testContext.assertFailure(baseTransactionDAO.saveTransactionsToPermanentTable("string", client))
-//      .onComplete(event -> {
-//        HttpStatusException exception = (HttpStatusException) event.cause();
-//        testContext.verify(() -> {
-//          assertEquals(exception.getStatusCode() , 400);
-//          assertEquals(exception.getPayload(), "Test");
-//        });
-//        testContext.completeNow();
-//      });
-//  }
+  @Test
+  void saveTransactionsToPermanentTable(Vertx vertx, VertxTestContext testContext) {
+    when(client.getPgClient()).thenReturn(postgresClient);
+    when(client.getConnection()).thenReturn(connection);
+    when(client.getTenantId()).thenReturn("test");
+
+    Mockito.when(baseTransactionDAO.createPermanentTransactionsQuery(anyString()))
+      .thenReturn("test.table");
+
+    doAnswer((Answer<Void>) invocation -> {
+      Handler<AsyncResult<RowSet<Row>>> handler = invocation.getArgument(3);
+      handler.handle(Future.failedFuture(new PgException("Test", "Test", "22P02", "Test")));
+      return null;
+    }).when(postgresClient).execute(eq(connection), eq("test.table"), any(Tuple.class), any(Handler.class));
+
+
+    testContext.assertFailure(baseTransactionDAO.saveTransactionsToPermanentTable(UUID.randomUUID().toString(), client))
+      .onComplete(event -> {
+        HttpStatusException exception = (HttpStatusException) event.cause();
+        testContext.verify(() -> {
+          assertEquals(exception.getStatusCode() , 400);
+          assertEquals(exception.getPayload(), "Test");
+        });
+        testContext.completeNow();
+      });
+  }
 
   @Test
   void updatePermanentTransactionsWithEmptyList() {
