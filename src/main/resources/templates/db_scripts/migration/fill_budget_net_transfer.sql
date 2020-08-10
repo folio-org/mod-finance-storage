1b7c8b74d5b44a1d7871591ce14764e15cd59597
Andrei_Makaranka <Andrei_makaranka@epam.com>
2020-08-10 10:29:56 +0300

    [MODFIN-119] - Migration script

---
+++ src/main/resources/templates/db_scripts/migration/fill_budget_net_transfer.sql
@@ -0,0 +1,17 @@
+ -- Update budgets
+UPDATE ${myuniversity}_${mymodule}.budget
+  SET
+    jsonb = jsonb || jsonb_build_object('netTransfer', totalNetTransfers.totalNetTransfer)
+  FROM
+		 (SELECT  calc.budgetId AS budgetId, sum(calc.calculatedNetTransfer) AS totalNetTransfer FROM
+				(SELECT budget.id as budgetId, budget.fundId as budgetFundId, transactions.fromFundId as transactionsFromFundId, transactions.toFundId as transactionsToFundId, transactions.fiscalYearId,
+							(CASE WHEN transactions.fromFundId = budget.fundId THEN -sum((transactions.jsonb->>'amount')::decimal)
+							 ELSE sum((transactions.jsonb->>'amount')::decimal) END) as calculatedNetTransfer
+					FROM ${myuniversity}_${mymodule}.transaction AS transactions
+						LEFT JOIN ${myuniversity}_${mymodule}.budget AS budget
+										ON (transactions.fromFundId = budget.fundId AND transactions.fiscalYearId = budget.fiscalYearId)
+												OR (transactions.toFundId = budget.fundId AND transactions.fiscalYearId = budget.fiscalYearId)
+					WHERE transactions.jsonb->>'transactionType'='Transfer'
+					GROUP BY budget.id, budget.fundId, transactions.fiscalYearId, transactions.fromFundId, transactions.toFundId) as calc
+		  GROUP BY calc.budgetId) as totalNetTransfers
+WHERE id = totalNetTransfers.budgetId;
