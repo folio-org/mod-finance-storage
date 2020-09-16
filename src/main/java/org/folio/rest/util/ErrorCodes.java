db70e836f813dd3e33045e98a205fcd589c04b8e
Yauheni Kuzmianok <yauheni_kuzmianok@epam.com>
2020-09-16 16:31:32 +0300

    [MODFISTO-142] Restrict transfer money if budget doesn't have enough available price
    Signed-off-by: Yauheni Kuzmianok <yauheni_kuzmianok@epam.com>

--- src/main/java/org/folio/rest/util/ErrorCodes.java
+++ src/main/java/org/folio/rest/util/ErrorCodes.java
@@ -4,7 +4,8 @@ import org.folio.rest.jaxrs.model.Error;
 
 public enum ErrorCodes {
   GENERIC_ERROR_CODE("genericError", "Generic error"),
-  UNIQUE_FIELD_CONSTRAINT_ERROR("uniqueField{0}{1}Error", "Field {0} must be unique");
+  UNIQUE_FIELD_CONSTRAINT_ERROR("uniqueField{0}{1}Error", "Field {0} must be unique"),
+  NOT_ENOUGH_MONEY_FOR_TRANSFER("notEnoughMoneyForTransferError", "Transfer was not successful. There is not enough money Available in the budget to complete this Transfer.");
 
   private final String code;
   private final String description;
