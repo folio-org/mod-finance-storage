b142d614f94d47a994f7e90583a81241c8505bd1
Siarhei Hrabko <45555481+grabsefx@users.noreply.github.com>
2020-09-19 21:32:05 +0300

    MODFISTO-136 updated encumbrances awaitingPayment calculation (#148)

--- src/test/java/org/folio/service/transactions/PendingPaymentServiceTest.java
+++ src/test/java/org/folio/service/transactions/PendingPaymentServiceTest.java
@@ -230,7 +230,7 @@ public class PendingPaymentServiceTest {
     verify(transactionsDAO).updatePermanentTransactions(encumbranceUpdateArgumentCapture.capture(), eq(client));
     Transaction updatedEncumbrance = encumbranceUpdateArgumentCapture.getValue().get(0);
     assertThat(updatedEncumbrance.getAmount(), is(0.0));
-    assertThat(updatedEncumbrance.getEncumbrance().getAmountAwaitingPayment(), is(10.1));
+    assertThat(updatedEncumbrance.getEncumbrance().getAmountAwaitingPayment(), is(0d));
 
     verify(transactionsDAO).saveTransactionsToPermanentTable(eq(summaryId), eq(client));
 
