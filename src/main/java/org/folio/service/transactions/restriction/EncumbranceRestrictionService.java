7dbb1a8521b29444beabfad700c7d14a624590fa
Damien <damien-git@users.noreply.github.com>
2021-01-29 12:45:26 -0500

    [MODFISTO-206] Changed calculation for EncumbranceRestrictionService.getBudgetRemainingAmount(), updated the other restriction services for consistency, added or updated unit tests

--- src/main/java/org/folio/service/transactions/restriction/EncumbranceRestrictionService.java
+++ src/main/java/org/folio/service/transactions/restriction/EncumbranceRestrictionService.java
@@ -39,18 +39,15 @@ public class EncumbranceRestrictionService extends BaseTransactionRestrictionSer
     Money allocated = Money.of(budget.getAllocated(), currency);
     // get allowableEncumbered converted from percentage value
     double allowableEncumbered = Money.of(budget.getAllowableEncumbrance(), currency).divide(100d).getNumber().doubleValue();
-    Money unavailable = Money.of(budget.getUnavailable(), currency);
-    Money available = Money.of(budget.getAvailable(), currency);
     Money encumbered = Money.of(budget.getEncumbered(), currency);
     Money awaitingPayment = Money.of(budget.getAwaitingPayment(), currency);
-    Money expenditures = Money.of(budget.getExpenditures(), currency);
+    Money expended = Money.of(budget.getExpenditures(), currency);
     Money netTransfers = Money.of(budget.getNetTransfers(), currency);
 
-    Money result = allocated.add(netTransfers).multiply(allowableEncumbered);
-    result = result.subtract(allocated.subtract(unavailable.add(available)));
-    result = result.subtract(encumbered.add(awaitingPayment).add(expenditures));
+    Money totalFunding = allocated.add(netTransfers);
+    Money unavailable = encumbered.add(awaitingPayment).add(expended);
 
-    return result;
+    return totalFunding.multiply(allowableEncumbered).subtract(unavailable);
   }
 
   @Override
--- src/main/java/org/folio/service/transactions/restriction/PaymentCreditRestrictionService.java
+++ src/main/java/org/folio/service/transactions/restriction/PaymentCreditRestrictionService.java
@@ -48,17 +48,16 @@ public class PaymentCreditRestrictionService extends BaseTransactionRestrictionS
     // get allowableExpenditure from percentage value
     double allowableExpenditure = Money.of(budget.getAllowableExpenditure(), currency).divide(100d).getNumber().doubleValue();
 
-    Money expenditure = Money.of(budget.getExpenditures(), currency);
+    Money expended = Money.of(budget.getExpenditures(), currency);
     Money relatedAwaitingPayment = relatedTransaction == null ? Money.of(0d, currency) : Money.of(relatedTransaction.getAmount(), currency);
     Money awaitingPayment = Money.of(budget.getAwaitingPayment(), currency);
     Money encumbered = Money.of(budget.getEncumbered(), currency);
     Money netTransfers = Money.of(budget.getNetTransfers(), currency);
 
-    Money result = allocated.add(netTransfers).multiply(allowableExpenditure);
+    Money totalFunding = allocated.add(netTransfers);
+    Money unavailable = encumbered.add(awaitingPayment).add(expended);
 
-    result = result.subtract(expenditure.add(encumbered).add(awaitingPayment).subtract(relatedAwaitingPayment));
-
-    return result;
+    return totalFunding.multiply(allowableExpenditure).subtract(unavailable).add(relatedAwaitingPayment);
   }
 
   @Override
