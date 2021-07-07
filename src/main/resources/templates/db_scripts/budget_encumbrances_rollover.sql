b9be30f710c51f136c7ec97fe8e596d29a09cb72
Andrei_Makaranka <Andrei_makaranka@epam.com>
2021-07-07 15:12:45 +0300

    [MODFISTO-247] - Fix version in schema.json

--- src/main/resources/templates/db_scripts/budget_encumbrances_rollover.sql
+++ src/main/resources/templates/db_scripts/budget_encumbrances_rollover.sql
@@ -32,10 +32,10 @@ CREATE OR REPLACE FUNCTION ${myuniversity}_${mymodule}.calculate_planned_encumbr
 		distribution_value DECIMAL DEFAULT 0;
 	BEGIN
 
-	  SELECT sum((jsonb->'encumbrance'->>'initialAmountEncumbered')::decimal) INTO po_line_cost
-	    FROM ${myuniversity}_${mymodule}.transaction
-        WHERE _rollover_record->>'fromFiscalYearId'=jsonb->>'fiscalYearId' AND jsonb->'encumbrance'->>'sourcePoLineId'=_transaction->'encumbrance'->>'sourcePoLineId'
-          GROUP BY jsonb->'encumbrance'->>'sourcePoLineId';
+	    SELECT sum((jsonb->'encumbrance'->>'initialAmountEncumbered')::decimal) INTO po_line_cost
+	        FROM ${myuniversity}_${mymodule}.transaction
+            WHERE _rollover_record->>'fromFiscalYearId'=jsonb->>'fiscalYearId' AND jsonb->'encumbrance'->>'sourcePoLineId'=_transaction->'encumbrance'->>'sourcePoLineId'
+            GROUP BY jsonb->'encumbrance'->>'sourcePoLineId';
 
     distribution_value := 0;
 
