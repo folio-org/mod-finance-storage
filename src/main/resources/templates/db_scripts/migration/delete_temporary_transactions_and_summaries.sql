8a4129b96fac25e3920abf46dd406a07fe49e861
Andriy_Kundyukov <Andriy_Kundyukov@epam.com>
2021-10-13 17:16:07 +0300

    MODFISTO-267 - delete from transaction summaries and temporary transactions

---
+++ src/main/resources/templates/db_scripts/migration/delete_temporary_transactions_and_summaries.sql
@@ -0,0 +1,7 @@
+DELETE FROM ${myuniversity}_${mymodule}.temporary_order_transactions;
+
+DELETE FROM ${myuniversity}_${mymodule}.temporary_invoice_transactions;
+
+DELETE FROM ${myuniversity}_${mymodule}.order_transaction_summaries;
+
+DELETE FROM ${myuniversity}_${mymodule}.invoice_transaction_summaries;
