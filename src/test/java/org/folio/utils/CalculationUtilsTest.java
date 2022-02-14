0241986e7e6223a2bf4e394d7a4559d2504ddb95
Andrei M <Andrei_Makaranka@epam.com>
2022-02-14 13:31:15 +0300

    [MODFISTO-283] - Over encumbered logic ready

--- src/test/java/org/folio/utils/CalculationUtilsTest.java
+++ src/test/java/org/folio/utils/CalculationUtilsTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.folio.rest.jaxrs.model.Budget;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CalculationUtilsTest {
 
@@ -18,21 +20,42 @@ public class CalculationUtilsTest {
         CalculationUtils.calculateBudgetSummaryFields(budget);
 
         assertEquals(8d, budget.getOverExpended(), 0d);
-
     }
 
+  @ParameterizedTest
+  @CsvSource({
+    "1800, 200, 1500, 500, 200, 200",
+    "1800, 200, 1500, 400, 100, 0",
+    "1800, 200, 1500, 600, 200, 200",
+    "1800, 200, 1000, 100, 2000, 0",
+    "1800, 200, 1000, 1000, 2000, 0",
+  })
+  void overEncumberedShouldBeGreaterThenZeroWhenEncumberedGreaterThatTotalFunding(double allocated, double netTransfer,
+                    double expended, double awaitingPayment, double encumbered, double expOverEncumbered) {
+    Budget budget = new Budget()
+      .withInitialAllocation(allocated)
+      .withNetTransfers(netTransfer)
+      .withExpenditures(expended)
+      .withAwaitingPayment(awaitingPayment)
+      .withEncumbered(encumbered);
+
+    CalculationUtils.calculateBudgetSummaryFields(budget);
+
+    assertEquals(expOverEncumbered, budget.getOverEncumbrance(), 0d);
+  }
+
+
   @Test
-  void overEncumberedShouldBeGreaterThenZeroWhenEncumberedGreaterThatTotalFunding() {
+  void overEncumberedShouldBeGreaterThenZeroWhenEncumberedAndOverExpendedGreaterThatTotalFunding() {
     Budget budget = new Budget()
       .withInitialAllocation(1800d)
       .withNetTransfers(200d)
       .withExpenditures(1500d)
-      .withAwaitingPayment(500d)
+      .withAwaitingPayment(600d)
       .withEncumbered(200d);
 
     CalculationUtils.calculateBudgetSummaryFields(budget);
 
     assertEquals(200d, budget.getOverEncumbrance(), 0d);
-
   }
 }
