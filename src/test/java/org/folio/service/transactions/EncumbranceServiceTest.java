ef4c3629016543d85631290a8f982f5a608a0ef5
Andrei M <Andrei_Makaranka@epam.com>
2022-08-01 11:23:57 +0200

    [MODFISTO-328] - Unit test updated

--- src/test/java/org/folio/service/transactions/EncumbranceServiceTest.java
+++ src/test/java/org/folio/service/transactions/EncumbranceServiceTest.java
@@ -5,12 +5,16 @@ import static org.folio.rest.RestConstants.OKAPI_URL;
 import static org.folio.rest.core.RestClientTest.X_OKAPI_TENANT;
 import static org.folio.rest.core.RestClientTest.X_OKAPI_TOKEN;
 import static org.folio.rest.core.RestClientTest.X_OKAPI_USER_ID;
+import static org.folio.rest.jaxrs.model.LedgerFiscalYearRolloverProgress.OverallRolloverStatus.IN_PROGRESS;
+import static org.folio.rest.jaxrs.model.LedgerFiscalYearRolloverProgress.OverallRolloverStatus.NOT_STARTED;
+import static org.folio.rest.jaxrs.model.LedgerFiscalYearRolloverProgress.OverallRolloverStatus.SUCCESS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -23,6 +27,7 @@ import org.folio.dao.transactions.TransactionDAO;
 import org.folio.rest.core.model.RequestContext;
 import org.folio.rest.jaxrs.model.Budget;
 import org.folio.rest.jaxrs.model.Encumbrance;
+import org.folio.rest.jaxrs.model.LedgerFiscalYearRolloverProgress;
 import org.folio.rest.jaxrs.model.Transaction;
 import org.folio.rest.persist.DBClient;
 import org.folio.rest.persist.DBClientFactory;
@@ -34,6 +39,7 @@ import org.folio.service.transactions.restriction.TransactionRestrictionService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.MockitoAnnotations;
 
 import io.vertx.core.Context;
@@ -90,12 +96,12 @@ public class EncumbranceServiceTest {
 
     Transaction tmpTransaction = new Transaction().withId(transactionId).withAmount(0.0).withCurrency("USD")
       .withFromFundId(fundId).withTransactionType(Transaction.TransactionType.ENCUMBRANCE)
-      .withEncumbrance(new Encumbrance().withSourcePurchaseOrderId(orderId)
+      .withEncumbrance(new Encumbrance().withStatus(Encumbrance.Status.RELEASED).withSourcePurchaseOrderId(orderId)
         .withInitialAmountEncumbered(10d).withAmountExpended(10d).withOrderStatus(Encumbrance.OrderStatus.OPEN));
 
     Transaction incomingTransaction = new Transaction().withId(transactionId).withAmount(0.0).withCurrency("USD")
       .withFromFundId(fundId).withTransactionType(Transaction.TransactionType.ENCUMBRANCE)
-      .withEncumbrance(new Encumbrance().withSourcePurchaseOrderId(orderId)
+      .withEncumbrance(new Encumbrance().withStatus(Encumbrance.Status.RELEASED).withSourcePurchaseOrderId(orderId)
         .withInitialAmountEncumbered(10d).withAmountExpended(10d).withOrderStatus(Encumbrance.OrderStatus.CLOSED));
 
     JsonObject trSummary = new JsonObject().put("id", orderId).put("numTransactions", 1);
@@ -111,19 +117,25 @@ public class EncumbranceServiceTest {
 
     doReturn(mockDBClient).when(mockDBClientFactory).getDbClient(eq(mockRequestContext));
     doReturn(succeededFuture(List.of(tmpTransaction))).when(mockTransactionDAO).getTransactions(any(Criterion.class), any(DBClient.class));
+    doReturn(succeededFuture(null)).when(mockTransactionDAO).updatePermanentTransactions(any(List.class), eq(mockDBClient));
+
     doReturn(succeededFuture(trSummary)).when(mockTransactionSummaryService).getAndCheckTransactionSummary(eq(incomingTransaction), any(DBClient.class));
     doReturn(orderId).when(mockTransactionSummaryService).getSummaryId(eq(incomingTransaction));
-    doReturn(succeededFuture(incomingTransaction)).when(mockTemporaryTransactionDAO).createTempTransaction(eq(incomingTransaction), eq(orderId), any(DBClient.class));
-    doReturn(succeededFuture(List.of(tmpTransaction))).when(mockTemporaryTransactionDAO).getTempTransactionsBySummaryId(eq(orderId), any(DBClient.class));
     doReturn(1).when(mockTransactionSummaryService).getNumTransactions(eq(trSummary));
-    doReturn(succeededFuture(null)).when(mockTransactionDAO).updatePermanentTransactions(any(List.class), eq(mockDBClient));
-    doReturn(succeededFuture(1)).when(mockTemporaryTransactionDAO).deleteTempTransactions(eq(orderId), any(DBClient.class));
     doReturn(succeededFuture(null)).when(mockTransactionSummaryService).setTransactionsSummariesProcessed(eq(trSummary), any(DBClient.class));
+
+    doReturn(succeededFuture(incomingTransaction)).when(mockTemporaryTransactionDAO).createTempTransaction(eq(incomingTransaction), eq(orderId), any(DBClient.class));
+    doReturn(succeededFuture(List.of(incomingTransaction))).when(mockTemporaryTransactionDAO).getTempTransactionsBySummaryId(eq(orderId), any(DBClient.class));
+    doReturn(succeededFuture(1)).when(mockTemporaryTransactionDAO).deleteTempTransactions(eq(orderId), any(DBClient.class));
 
     encumbranceService.updateTransaction(incomingTransaction, mockRequestContext)
       .onComplete(event -> {
         testContext.verify(() -> {
-          assertEquals(true, true);
+          ArgumentCaptor<List> argumentCaptor = ArgumentCaptor.forClass(List.class);
+          verify(mockTransactionDAO).updatePermanentTransactions(argumentCaptor.capture(), eq(mockDBClient));
+          List<Transaction> transactions = argumentCaptor.getValue();
+          assertEquals(Encumbrance.OrderStatus.CLOSED, transactions.get(0).getEncumbrance().getOrderStatus());
+          assertEquals(Encumbrance.Status.RELEASED, transactions.get(0).getEncumbrance().getStatus());
         });
         testContext.completeNow();
       });
