572ef7485be21f8ab12f156431f2eb130e429a1d
Abdulkhakimov <89521577+Abdulkhakimov@users.noreply.github.com>
2022-08-09 19:49:17 +0500

    [MODFISTO-320] Define and implement Storage API : Ledger Rollover Budgets (#276)

---
+++ src/main/java/org/folio/rest/impl/LegderRolloverBudgetAPI.java
@@ -0,0 +1,33 @@
+package org.folio.rest.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Handler;
+import org.folio.rest.annotations.Validate;
+import org.folio.rest.jaxrs.model.LedgerFiscalYearRolloverBudget;
+import org.folio.rest.jaxrs.resource.FinanceStorageLedgerRolloversBudgets;
+import org.folio.rest.jaxrs.model.LedgerFiscalYearRolloverBudgetCollection;
+import org.folio.rest.persist.PgUtil;
+
+import javax.ws.rs.core.Response;
+import java.util.Map;
+
+public class LegderRolloverBudgetAPI implements FinanceStorageLedgerRolloversBudgets {
+
+  public static final String LEDGER_FISCAL_YEAR_ROLLOVER_BUDGETS_TABLE = "ledger_fiscal_year_rollover_budget";
+
+  @Override
+  @Validate
+  public void getFinanceStorageLedgerRolloversBudgets(String query, int offset, int limit, String lang, Map<String, String> okapiHeaders,
+              Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.get(LEDGER_FISCAL_YEAR_ROLLOVER_BUDGETS_TABLE, LedgerFiscalYearRolloverBudget.class, LedgerFiscalYearRolloverBudgetCollection.class, query,
+      offset, limit, okapiHeaders, vertxContext, FinanceStorageLedgerRolloversBudgets.GetFinanceStorageLedgerRolloversBudgetsResponse.class, asyncResultHandler);
+  }
+
+  @Override
+  @Validate
+  public void getFinanceStorageLedgerRolloversBudgetsById(String id, String lang, Map<String, String> okapiHeaders,
+              Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    PgUtil.getById(LEDGER_FISCAL_YEAR_ROLLOVER_BUDGETS_TABLE, LedgerFiscalYearRolloverBudget.class, id, okapiHeaders, vertxContext, FinanceStorageLedgerRolloversBudgets.GetFinanceStorageLedgerRolloversBudgetsByIdResponse.class, asyncResultHandler);
+  }
+}
