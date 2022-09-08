b0da67737a6bfbb3bc43628abc68820f2267ba48
Dzmitry_Butramyou <dzmitry_butramyou@epam.com>
2022-09-08 16:31:21 +0400

    [MODFISTO-327] - Refactoring

--- src/main/java/org/folio/rest/impl/LedgerRolloverAPI.java
+++ src/main/java/org/folio/rest/impl/LedgerRolloverAPI.java
@@ -59,7 +59,7 @@ public class LedgerRolloverAPI implements FinanceStorageLedgerRollovers {
                 if (Response.Status.CONFLICT.getStatusCode() == cause.getStatusCode()) {
                   log.error("Rollover already exists with id {}", entity.getId(), cause);
                 } else {
-                  log.error("Update of the fund record {} has failed", entity.getId(), cause);
+                  log.error("Creating rollover with id {} failed", entity.getId(), cause);
                 }
                 HelperUtils.replyWithErrorResponse(asyncResultHandler, cause);
               } else {
