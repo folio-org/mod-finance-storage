216df1fdda1455978a58f6b8ba629b0d5f0bd7cc
Khamidulla Abdulkhakimov <Khamidulla_Abdulkhakimov@epam.com>
2022-09-23 17:06:54 +0500

    [MODFIN-270] Changed status type for combining log result

--- src/main/resources/templates/db_scripts/ledger_rollover_log_view.sql
+++ src/main/resources/templates/db_scripts/ledger_rollover_log_view.sql
@@ -3,10 +3,10 @@ AS SELECT rollover.id AS id,
           jsonb_build_object('ledgerRolloverId', rollover.id) ||
           jsonb_build_object('startDate', progress.jsonb #>> '{metadata, createdDate}') ||
           CASE
-              WHEN progress.jsonb ->> 'ordersRolloverStatus' IN ('Not Started', 'In Progress') THEN '{}'::jsonb
+              WHEN progress.jsonb ->> 'overallRolloverStatus' IN ('Not Started', 'In Progress') THEN '{}'::jsonb
               ELSE jsonb_build_object('endDate', progress.jsonb #>> '{metadata, updatedDate}')
           END ||
-          jsonb_build_object('rolloverStatus', progress.jsonb ->> 'ordersRolloverStatus') ||
+          jsonb_build_object('rolloverStatus', progress.jsonb ->> 'overallRolloverStatus') ||
           jsonb_build_object('ledgerRolloverType', rollover.jsonb ->> 'rolloverType') AS jsonb
 FROM ${myuniversity}_${mymodule}.ledger_fiscal_year_rollover AS rollover
          INNER JOIN ${myuniversity}_${mymodule}.ledger_fiscal_year_rollover_progress AS progress on rollover.id = progress.ledgerrolloverid;
