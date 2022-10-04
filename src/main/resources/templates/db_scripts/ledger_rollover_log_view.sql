9e365673647d91bf1e56e2b4336f59b3ce2eaf5d
Serhii Nosko <Serhii_Nosko@epam.com>
2022-10-04 18:18:06 +0300

    [MODFIN-277]. Add support to filter by ledger for Rollover Logs API (#304)

--- src/main/resources/templates/db_scripts/ledger_rollover_log_view.sql
+++ src/main/resources/templates/db_scripts/ledger_rollover_log_view.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE VIEW ${myuniversity}_${mymodule}.ledger_rollover_logs_view
 AS SELECT rollover.id AS id,
           jsonb_build_object('ledgerRolloverId', rollover.id) ||
+          jsonb_build_object('ledgerId', rollover.ledgerId) ||
           jsonb_build_object('startDate', progress.jsonb #>> '{metadata, createdDate}') ||
           CASE
               WHEN progress.jsonb ->> 'overallRolloverStatus' IN ('Not Started', 'In Progress') THEN '{}'::jsonb
