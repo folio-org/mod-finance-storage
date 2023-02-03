f9bf3567672b92226dd781a761634c9745da3700
Dzmitry_Butramyou <dzmitry_butramyou@epam.com>
2023-02-03 17:43:31 +0400

    [MODFISTO-373] - Migration for update budget rollover added

--- src/main/resources/templates/db_scripts/migration/update_budget_rollover.sql
+++ src/main/resources/templates/db_scripts/migration/update_budget_rollover.sql
@@ -1,7 +1,7 @@
 UPDATE ${myuniversity}_${mymodule}.ledger_fiscal_year_rollover
 SET jsonb =
       (
-        -- Update each budgetsRollover element renaming 'rolloverAvailable' to 'rolloverBasedOn' and if field true update to 'Allocation' else to 'None'
+        -- Update each budgetsRollover element renaming 'rolloverAvailable' to 'rolloverBudgetValue' and if field true update to 'Allocation' else to 'None'
         SELECT jsonb_set(jsonb, '{budgetsRollover}', jsonb_agg(rollover - 'rolloverAvailable'
           || jsonb_build_object('rolloverBudgetValue',
                                 CASE WHEN (rollover -> 'rolloverAvailable')::boolean
