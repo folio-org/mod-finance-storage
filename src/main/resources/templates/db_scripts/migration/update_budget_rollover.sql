d0fd6cf2e8ab26a53f0dfe06b459d6062dad653e
Dzmitry_Butramyou <dzmitry_butramyou@epam.com>
2023-02-07 11:26:37 +0400

    [MODFISTO-373] - Script updated

--- src/main/resources/templates/db_scripts/migration/update_budget_rollover.sql
+++ src/main/resources/templates/db_scripts/migration/update_budget_rollover.sql
@@ -5,7 +5,7 @@ SET jsonb =
         SELECT jsonb_set(jsonb, '{budgetsRollover}', jsonb_agg(rollover - 'rolloverAvailable'
           || jsonb_build_object('rolloverBudgetValue',
                                 CASE WHEN (rollover -> 'rolloverAvailable')::boolean
-                                     THEN 'Allocation'
+                                     THEN 'Available'
                                      ELSE 'None'
                                   END)))
         FROM jsonb_array_elements(jsonb -> 'budgetsRollover') rollover
