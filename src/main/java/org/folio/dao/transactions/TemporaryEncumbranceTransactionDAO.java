1eca88cc88c1c6b49352e7a554e577568821cbe4
Khamidulla Abdulkhakimov <Khamidulla_Abdulkhakimov@epam.com>
2023-02-15 19:42:58 +0500

    [MODFISTO-377] Fixed tests

--- src/main/java/org/folio/dao/transactions/TemporaryEncumbranceTransactionDAO.java
+++ src/main/java/org/folio/dao/transactions/TemporaryEncumbranceTransactionDAO.java
@@ -1,18 +1,22 @@
 package org.folio.dao.transactions;
 
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
 import org.folio.rest.jaxrs.model.Transaction;
 import org.folio.rest.persist.Conn;
 import org.folio.rest.persist.Criteria.Criterion;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.String.format;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.folio.rest.util.ResponseUtils.handleFailure;
 
 public class TemporaryEncumbranceTransactionDAO extends BaseTemporaryTransactionsDAO{
 
   public static final String TEMPORARY_ENCUMBRANCE_TRANSACTIONS_TABLE = "tmp_encumbered_transactions";
+  private static final String TEMPORARY_ENCUMBRANCE_TRANSACTIONS_QUERY = "SELECT jsonb FROM tmp_encumbered_transactions WHERE %s ";
 
   public TemporaryEncumbranceTransactionDAO() {
     super(TEMPORARY_ENCUMBRANCE_TRANSACTIONS_TABLE);
@@ -30,9 +34,17 @@ public class TemporaryEncumbranceTransactionDAO extends BaseTemporaryTransaction
 
   @Override
   public Future<List<Transaction>> getTempTransactions(Criterion criterion, Conn conn) {
-    return conn.get(TEMPORARY_ENCUMBRANCE_TRANSACTIONS_TABLE, Transaction.class, criterion)
-      .transform(reply -> reply.failed() ? Future.future(promise -> handleFailure(promise, reply))
-        : Future.succeededFuture(reply.result().getResults()));
+    return conn.execute(format(TEMPORARY_ENCUMBRANCE_TRANSACTIONS_QUERY, criterion.getWhere()))
+      .transform(reply -> {
+        if (reply.failed()) {
+          return Future.future(promise -> handleFailure(promise, reply));
+        } else {
+          List<Transaction> transactions = new ArrayList<>();
+          reply.result().spliterator()
+            .forEachRemaining(row -> transactions.add(row.get(JsonObject.class, 0).mapTo(Transaction.class)));
+          return Future.succeededFuture(transactions);
+        }
+      });
   }
 
 }
