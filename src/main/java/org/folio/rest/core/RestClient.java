7c4a49f8e86a313223603323097bbc176b0c8d7f
Julian Ladisch <julianladisch@users.noreply.github.com>
2023-03-06 21:07:12 +0100

    MODFISTO-411: RMB HttpClient resource leak, use Vert.x WebClient
    https://issues.folio.org/browse/MODFISTO-411
    
    mod-finance-storage uses RMB's HttpClient (via HttpClientFactory, HttpClientInterface, HttpClientMock2) that causes resource leaks because it doesn't reuses HTTP connections and doesn't always close HTTP connections. It has been deprecated since April 2021: https://github.com/folio-org/raml-module-builder/commit/c827177d10713aa9c03866ac5234ec136eb471d5
    
    Replace it with Vert.x WebClient.

--- src/main/java/org/folio/rest/core/RestClient.java
+++ src/main/java/org/folio/rest/core/RestClient.java
@@ -3,171 +3,91 @@ package org.folio.rest.core;
 import static java.util.Objects.nonNull;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
+import static org.folio.rest.RestConstants.OKAPI_URL;
 import static org.folio.rest.RestVerticle.OKAPI_HEADER_TENANT;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.EnumSet;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.CompletionException;
+import static org.folio.rest.RestVerticle.OKAPI_HEADER_TOKEN;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.folio.okapi.common.WebClientFactory;
 import org.folio.rest.RestConstants;
 import org.folio.rest.core.model.RequestContext;
-import org.folio.rest.exception.HttpException;
-import org.folio.rest.tools.client.HttpClientFactory;
-import org.folio.rest.tools.client.Response;
-import org.folio.rest.tools.client.interfaces.HttpClientInterface;
-import org.folio.rest.tools.utils.TenantTool;
-import org.folio.rest.util.ErrorCodes;
-
+import org.folio.util.PercentCodec;
 import io.vertx.core.Future;
-import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.predicate.ResponsePredicate;
 
 public class RestClient {
   private static final Logger LOGGER = LogManager.getLogger(RestClient.class);
-  private static final String ERROR_MESSAGE = "errorMessage";
   private static final String SEARCH_ENDPOINT = "%s?limit=%s&offset=%s%s";
   private static final String EXCEPTION_CALLING_ENDPOINT_MSG = "Exception calling {} {} {}";
-
   private final String baseEndpoint;
+  private final Vertx vertx = Vertx.currentContext() == null ? Vertx.vertx() : Vertx.currentContext().owner();
+  private final WebClient webClient = WebClientFactory.getWebClient(vertx);
 
   public RestClient(String baseEndpoint) {
     this.baseEndpoint = baseEndpoint;
   }
 
-  public <T> Future<T> get(String query, int offset, int limit, RequestContext requestContext, Class<T> responseType) {
-    String endpoint = String.format(SEARCH_ENDPOINT, baseEndpoint, limit, offset, buildQueryParam(query, LOGGER));
-    return get(requestContext, endpoint, responseType);
+  public Future<JsonObject> get(String query, int offset, int limit, RequestContext requestContext) {
+    return get(requestContext, String.format(SEARCH_ENDPOINT, baseEndpoint, limit, offset, buildQueryParam(query)));
   }
 
-  public <T> Future<T> getById(String id, RequestContext requestContext, Class<T> responseType) {
-    String endpoint = String.format("%s/%s", baseEndpoint, id);
-    return get(requestContext, endpoint, responseType);
+  public Future<JsonObject> getById(String id, RequestContext requestContext) {
+    return get(requestContext, baseEndpoint + "/" + id);
   }
 
-  private <T> Future<T> get(RequestContext requestContext, String endpoint, Class<T> responseType) {
-    Promise<T> promise = Promise.promise();
-    HttpClientInterface client = getHttpClient(requestContext.getHeaders());
-    if (LOGGER.isDebugEnabled()) {
-      LOGGER.debug("Calling GET {}", endpoint);
-    }
+  private Future<JsonObject> get(RequestContext requestContext, String endpoint) {
     try {
-      client
-        .request(HttpMethod.GET, endpoint, requestContext.getHeaders())
-        .thenApply(response -> {
-          if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("Validating response for GET {}", endpoint);
-          }
-          return verifyAndExtractBody(response);
-        })
-        .handle((body, t) -> {
-          client.closeClient();
-          if (t != null) {
-            LOGGER.error(EXCEPTION_CALLING_ENDPOINT_MSG, t, HttpMethod.GET, endpoint);
-            promise.fail(t.getCause());
-          } else {
+      LOGGER.debug("Calling GET {}", endpoint);
+      String url = requestContext.getHeaders().get(RestConstants.OKAPI_URL) + endpoint;
+      return webClient.getAbs(url)
+          .putHeader(OKAPI_HEADER_TENANT, requestContext.getHeaders().get(OKAPI_HEADER_TENANT))
+          .putHeader(OKAPI_HEADER_TOKEN, requestContext.getHeaders().get(OKAPI_HEADER_TOKEN))
+          .expect(ResponsePredicate.SC_OK)
+          .send()
+          .map(HttpResponse::bodyAsJsonObject)
+          .onSuccess(body -> {
             if (LOGGER.isDebugEnabled()) {
               LOGGER.debug("The response body for GET {}: {}", endpoint, nonNull(body) ? body.encodePrettily() : null);
             }
-            if (JsonObject.class.equals(responseType)) {
-              promise.complete((T) body);
-            } else {
-              T responseEntity = body.mapTo(responseType);
-              promise.complete(responseEntity);
-            }
-          }
-          return null;
-        });
+          })
+          .onFailure(e -> LOGGER.error(EXCEPTION_CALLING_ENDPOINT_MSG, HttpMethod.GET, endpoint, e.getMessage()));
     } catch (Exception e) {
-      LOGGER.error(EXCEPTION_CALLING_ENDPOINT_MSG, e, HttpMethod.GET, baseEndpoint);
-      client.closeClient();
-      promise.fail(e);
+      LOGGER.error(EXCEPTION_CALLING_ENDPOINT_MSG, HttpMethod.GET, endpoint, e.getMessage(), e);
+      return Future.failedFuture(e);
     }
-    return promise.future();
   }
 
   public <T> Future<Void> postEmptyResponse(T entity, RequestContext requestContext) {
-    Promise<Void> promise = Promise.promise();
-    String endpoint = baseEndpoint;
-    JsonObject recordData = JsonObject.mapFrom(entity);
-
-    if (LOGGER.isDebugEnabled()) {
-      LOGGER.debug("Sending 'POST {}' with body: {}", endpoint, recordData.encodePrettily());
-    }
-
-    HttpClientInterface client = getHttpClient(requestContext.getHeaders());
     try {
-      client.request(HttpMethod.POST, recordData.toBuffer(), endpoint, requestContext.getHeaders())
-        .thenAccept(this::verifyResponse)
-        .handle((body, t) -> {
-          client.closeClient();
-          if (t != null) {
-            LOGGER.error("'POST {}' request failed. Request body: {}", endpoint, recordData.encodePrettily(), t.getCause());
-            promise.fail(t.getCause());
-          } else {
-            if (LOGGER.isDebugEnabled()) {
-              LOGGER.debug("'POST {}' request successfully processed. Record with '{}' id has been created", endpoint, body);
-            }
-            promise.complete();
-          }
-          return null;
-        });
+      JsonObject recordData = JsonObject.mapFrom(entity);
+
+      if (LOGGER.isDebugEnabled()) {
+        LOGGER.debug("Sending 'POST {}' with body: {}", baseEndpoint, recordData.encodePrettily());
+      }
+
+      return webClient.postAbs(requestContext.getHeaders().get(OKAPI_URL) + baseEndpoint)
+          .putHeader(OKAPI_HEADER_TENANT, requestContext.getHeaders().get(OKAPI_HEADER_TENANT))
+          .putHeader(OKAPI_HEADER_TOKEN, requestContext.getHeaders().get(OKAPI_HEADER_TOKEN))
+          .expect(ResponsePredicate.status(200, 299))
+          .sendJsonObject(recordData)
+          .onSuccess(body -> LOGGER.debug(
+              "'POST {}' request successfully processed. Record with '{}' id has been created", baseEndpoint, body))
+          .onFailure(e -> LOGGER.error("'POST {}' request failed: {}. Request body: {}",
+              baseEndpoint, e.getCause(), recordData.encodePrettily()))
+          .mapEmpty();
     } catch (Exception e) {
-      LOGGER.error("'POST {}' request failed. Request body: {}", endpoint, recordData.encodePrettily(), e);
-      client.closeClient();
-      promise.fail(e);
-    }
-
-    return promise.future();
-  }
-
-  public HttpClientInterface getHttpClient(Map<String, String> okapiHeaders) {
-    final String okapiURL = okapiHeaders.getOrDefault(RestConstants.OKAPI_URL, "");
-    final String tenantId = TenantTool.calculateTenantId(okapiHeaders.get(OKAPI_HEADER_TENANT));
-
-    return HttpClientFactory.getHttpClient(okapiURL, tenantId);
-
-  }
-
-  private void verifyResponse(Response response) {
-    if (!Response.isSuccess(response.getCode())) {
-      String errorMsg = response.getError()
-        .getString(ERROR_MESSAGE);
-      HttpException httpException = getErrorByCode(errorMsg).map(errorCode -> new HttpException(response.getCode(), errorCode))
-        .orElse(new HttpException(response.getCode(), errorMsg));
-      throw new CompletionException(httpException);
+      LOGGER.error("'POST {}' request failed: {}.", baseEndpoint, e.getCause());
+      return Future.failedFuture(e);
     }
   }
 
-  private Optional<ErrorCodes> getErrorByCode(String errorCode) {
-    return EnumSet.allOf(ErrorCodes.class)
-      .stream()
-      .filter(errorCodes -> errorCodes.getCode()
-        .equals(errorCode))
-      .findAny();
-  }
-
-  private String encodeQuery(String query, Logger logger) {
-    try {
-      return URLEncoder.encode(query, StandardCharsets.UTF_8.toString());
-    } catch (UnsupportedEncodingException e) {
-      logger.error("Error happened while attempting to encode '{}'", query);
-      throw new CompletionException(e);
-    }
-  }
-
-  private String buildQueryParam(String query, Logger logger) {
-    return isEmpty(query) ? EMPTY : "&query=" + encodeQuery(query, logger);
-  }
-
-  private JsonObject verifyAndExtractBody(Response response) {
-    verifyResponse(response);
-    return response.getBody();
+  private String buildQueryParam(String query) {
+    return isEmpty(query) ? EMPTY : "&query=" + PercentCodec.encode(query);
   }
 }
