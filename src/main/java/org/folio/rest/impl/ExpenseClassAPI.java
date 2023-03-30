2b00cd4aa8cb70fa75bc65b12156b6e59bff7767
Bekhzod Kurbonboev <bekhzod_kurbonboev@epam.com>
2023-03-31 02:20:44 +0500

    Remove 'javax.validation.constraints.*' annotations in implementation

--- src/main/java/org/folio/rest/impl/ExpenseClassAPI.java
+++ src/main/java/org/folio/rest/impl/ExpenseClassAPI.java
@@ -2,9 +2,6 @@ package org.folio.rest.impl;
 
 import java.util.Map;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Pattern;
 import javax.ws.rs.core.Response;
 
 import org.folio.rest.annotations.Validate;
@@ -30,8 +27,8 @@ public class ExpenseClassAPI implements FinanceStorageExpenseClasses {
 
   @Override
   @Validate
-  public void getFinanceStorageExpenseClasses(String query, String totalRecords, @Min(0) @Max(2147483647) int offset, @Min(0) @Max(2147483647) int limit,
-      Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+  public void getFinanceStorageExpenseClasses(String query, String totalRecords, int offset, int limit, Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     PgUtil.get(EXPENSE_CLASS_TABLE, ExpenseClass.class, ExpenseClassCollection.class, query, offset, limit, okapiHeaders,
         vertxContext, FinanceStorageExpenseClasses.GetFinanceStorageExpenseClassesResponse.class, asyncResultHandler);
   }
@@ -45,27 +42,24 @@ public class ExpenseClassAPI implements FinanceStorageExpenseClasses {
 
   @Override
   @Validate
-  public void getFinanceStorageExpenseClassesById(
-      @Pattern(regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$") String id,
-      Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+  public void getFinanceStorageExpenseClassesById(String id, Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     PgUtil.getById(EXPENSE_CLASS_TABLE, ExpenseClass.class, id, okapiHeaders, vertxContext,
         FinanceStorageExpenseClasses.GetFinanceStorageExpenseClassesByIdResponse.class, asyncResultHandler);
   }
 
   @Override
   @Validate
-  public void deleteFinanceStorageExpenseClassesById(
-      @Pattern(regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$") String id,
-      Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+  public void deleteFinanceStorageExpenseClassesById(String id, Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     PgUtil.deleteById(EXPENSE_CLASS_TABLE, id, okapiHeaders, vertxContext,
         FinanceStorageExpenseClasses.DeleteFinanceStorageExpenseClassesByIdResponse.class, asyncResultHandler);
   }
 
   @Override
   @Validate
-  public void putFinanceStorageExpenseClassesById(
-      @Pattern(regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$") String id,
-      ExpenseClass entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+  public void putFinanceStorageExpenseClassesById(String id, ExpenseClass entity, Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     expenseClassService.updateExpenseClass(id, entity, vertxContext, asyncResultHandler);
   }
 }
