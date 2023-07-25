3a2ed29f076e480700947832bd9d0618305e5ddf
Tatsiana Tarhonskaya <tatsiana_tarhonskaya@epam.com>
2023-07-25 19:29:33 +0400

    MODFISTO-439 Make adjustments to improve finance rollover script performance (#364)
    * MODFISTO-439 Make adjustments to improve finance rollover script performance
    
    * MODFISTO-439 Make adjustments to improve finance rollover script performance. Part2
    
    * MODFISTO-439 Add index for encumbrance.sourcePoLineId
    
    * MODFISTO-439 modify queries using "::text" where possible to promote indexes usage
    
    * MODFISTO-439 extract _rollover_record fields to variables
    
    * MODFISTO-439 extract _rollover_record fields to variables.prt 2
    
    * MODFISTO-439 extract _rollover_record fields to variables.prt 3
    
    * MODFISTO-439 extract _rollover_record fields to variables.prt 4
    
    * MODFISTO-439 extract _rollover_record fields to variables.prt 5
    
    * MODFISTO-439 update declarations
    
    * MODFISTO-439 revert changes to temp tables queries(there's only jsonb and id, no other columns. Also no indexes)
    
    * MODFISTO-439 update encumbrance.sourcePoLineId index to btree type, minor script updates
    
    * MODFISTO-439 revert change in update_budget_amounts_query
    
    * MODFISTO-439 revert change in query to tmp table
    
    * MODFISTO-439 update subquery to use indexes
    
    * MODFISTO-439 remove ledgerRolloverId and fiscalYearId index definitions to replace them by default indexes for foreign key columns
    
    * MODFISTO-439 add additional indexes to have both btree and gin indexes for encumbrance.sourcePurchaseOrderId and encumbrance.sourcePoLineId
    
    * MODFISTO-439 add to_btree_format function and update queries containing sourcePoLineId to use index
    
    * MODFISTO-439 setting log_statement to 'all' for debug purpose
    
    * MODFISTO-439 remove log statement
    
    * MODFISTO-439 update queries with encumbrance sourcePurchaseOrderId to use index
    
    * MODFISTO-439 fix query with transaction to use indexes
    
    * MODFISTO-439 fix formatting
    
    * MODFISTO-439 test
    
    * Revert "MODFISTO-439 test"
    
    This reverts commit ea7517bdd2fcc701cce0edd582f79bee10932700.
    
    * MODFISTO-439 update "fromModuleVersion" for transaction and ledger_fiscal_year_rollover_budget tables
    
    * Revert "MODFISTO-439 update "fromModuleVersion" for transaction and ledger_fiscal_year_rollover_budget tables"
    
    This reverts commit 5d31091c674a3bf08711f052aacfb0e0853ec794.
    
    * MODFISTO-439 remove "fromModuleVersion" - attempt to fix indexes creation
    
    * MODFISTO-439 add script for removing indexes from rmb_internal_index
    
    * MODFISTO-439 restore "fromModuleVersion"
    
    * MODFISTO-439 rename script
    
    * MODFISTO-439 add schema name to function
    
    * MODFISTO-439 formatting update

---
+++ src/main/resources/templates/db_scripts/remove_from_rmb_internal_index.sql
@@ -0,0 +1,1 @@
+DELETE from ${myuniversity}_${mymodule}.rmb_internal_index where name in ('ledger_fiscal_year_rollover_budget_ledgerRolloverId_idx', 'ledger_fiscal_year_rollover_budget_fiscalYearId_idx');
