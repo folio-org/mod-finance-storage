4d7ab4f91b2c22d45494429a2edcfc11bd19c4d0
Khamidulla Abdulkhakimov <Khamidulla_Abdulkhakimov@epam.com>
2024-02-28 18:11:27 +0500

    [MODFISTO-461] - Modified approach

--- src/main/resources/templates/db_scripts/transactions.sql
+++ src/main/resources/templates/db_scripts/transactions.sql
@@ -1,3 +1,6 @@
 DROP TRIGGER IF EXISTS recalculate_totals on ${myuniversity}_${mymodule}."transaction";
 DROP FUNCTION IF EXISTS ${myuniversity}_${mymodule}.recalculate_totals();
 
+ALTER TABLE ${myuniversity}_${mymodule}."transaction" DROP CONSTRAINT IF EXISTS transaction_encumbrance_unique;
+ALTER TABLE ${myuniversity}_${mymodule}."transaction" ADD CONSTRAINT transaction_encumbrance_unique UNIQUE USING INDEX transaction_encumbrance_idx_unique;
+
