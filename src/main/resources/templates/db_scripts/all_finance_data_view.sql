ee11fc111eb8c44f790de8741699124f6599dcba
Serhii_Nosko <Serhii_Nosko@epam.com>
2025-02-26 15:49:21 +0200

    MODFISTO-519. Only one row for unique budget should be displayed in batch allocation form if fund is included in different groups

--- src/main/resources/templates/db_scripts/all_finance_data_view.sql
+++ src/main/resources/templates/db_scripts/all_finance_data_view.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE VIEW ${myuniversity}_${mymodule}.finance_data_view AS
 SELECT
+  DISTINCT ON (fiscal_year.id, fund.id, budget.id)
   fiscal_year.id as id,
   jsonb_build_object(
     'fiscalYearId', fiscal_year.id,
@@ -36,4 +37,5 @@ LEFT OUTER JOIN ${myuniversity}_${mymodule}.budget
 LEFT OUTER JOIN ${myuniversity}_${mymodule}.group_fund_fiscal_year
     ON group_fund_fiscal_year.fundid = fund.id AND group_fund_fiscal_year.fiscalYearId = fiscal_year.id AND group_fund_fiscal_year.budgetId = budget.id
 LEFT OUTER JOIN ${myuniversity}_${mymodule}.groups
-    ON groups.id = group_fund_fiscal_year.groupid;
+    ON groups.id = group_fund_fiscal_year.groupid
+ORDER BY fiscal_year.id, fund.id, budget.id;
