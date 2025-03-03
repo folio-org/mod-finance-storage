fb1913fcdae0c7a91cacb168fc538a38aa490dad
Serhii Nosko <Serhii_Nosko@epam.com>
2025-03-03 13:37:01 +0200

    MODFISTO-519. Only one row for unique budget should be displayed in batch allocation form if fund is included in different groups (#457)
    * MODFISTO-519. Only one row for unique budget should be displayed in batch allocation form if fund is included in different groups

--- src/test/java/org/folio/rest/impl/FinanceDataApiTest.java
+++ src/test/java/org/folio/rest/impl/FinanceDataApiTest.java
@@ -28,6 +28,7 @@ import org.folio.rest.jaxrs.model.TenantJob;
 import org.folio.rest.jaxrs.resource.FinanceStorageFinanceData;
 import org.folio.rest.persist.HelperUtils;
 import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -38,7 +39,7 @@ public class FinanceDataApiTest extends TestBase {
   private static final Logger logger = LogManager.getLogger();
   private static final String FINANCE_DATA_ENDPOINT = HelperUtils.getEndpoint(FinanceStorageFinanceData.class);
   private static TenantJob tenantJob;
-  private static final String FISCAL_YEAR_ID = UUID.randomUUID().toString();
+  private static final String FINANCE_DATA_ACQ_ENDPOINT_FORMAT = "%s?query=(fiscalYearId==%s and fundAcqUnitIds=%s and budgetAcqUnitIds=%s)";
 
   @BeforeAll
   public static void before() {
@@ -55,13 +56,16 @@ public class FinanceDataApiTest extends TestBase {
 
   @Test
   public void positive_testGetQuery() {
-    verifyCollectionQuantity(FINANCE_DATA_ENDPOINT + "?query=(fiscalYearId==7a4c4d30-3b63-4102-8e2d-3ee5792d7d02)", 24, TENANT_HEADER);
-    verifyCollectionQuantity(FINANCE_DATA_ENDPOINT + "?query=(fiscalYearId==9b1d00d1-1f3d-4f1c-8e4b-0f1e3b7b1b1b)", 0, TENANT_HEADER);
+    var emptyResponse = getData(FINANCE_DATA_ENDPOINT + "?query=(fiscalYearId==9b1d00d1-1f3d-4f1c-8e4b-0f1e3b7b1b1b)", TENANT_HEADER);
+    var emptyBody = emptyResponse.getBody().as(FyFinanceDataCollection.class);
+
+    assertTrue(emptyBody.getFyFinanceData().isEmpty());
 
     var response = getData(FINANCE_DATA_ENDPOINT + "?query=(fiscalYearId==7a4c4d30-3b63-4102-8e2d-3ee5792d7d02)", TENANT_HEADER);
     var body = response.getBody().as(FyFinanceDataCollection.class);
     var actualFyFinanceData = body.getFyFinanceData().get(0);
 
+    assertTrue(body.getFyFinanceData().size() > 1);
     assertEquals("7a4c4d30-3b63-4102-8e2d-3ee5792d7d02", actualFyFinanceData.getFiscalYearId());
     assertNotNull(actualFyFinanceData.getFundId());
     assertNotNull(actualFyFinanceData.getFundCode());
@@ -81,16 +85,17 @@ public class FinanceDataApiTest extends TestBase {
 
   @Test
   public void positive_testResponseOfGetWithParamsFiscalYearAndAcqUnitIds() {
+    var fiscalYearId = UUID.randomUUID().toString();
     var acqUnitId = UUID.randomUUID().toString();
-    var fiscalYearAcqUnitEndpoint = String.format("%s?query=(fiscalYearId==%s and fundAcqUnitIds=%s and budgetAcqUnitIds=%s)",
-      FINANCE_DATA_ENDPOINT, FISCAL_YEAR_ID, acqUnitId, acqUnitId);
-    createMockData(FISCAL_YEAR_ID, acqUnitId, "FY2088", "first");
+    var financeDataEndpoint = String.format(FINANCE_DATA_ACQ_ENDPOINT_FORMAT, FINANCE_DATA_ENDPOINT, fiscalYearId, acqUnitId, acqUnitId);
 
-    var response = getData(fiscalYearAcqUnitEndpoint, TENANT_HEADER);
+    createMockData(fiscalYearId, acqUnitId, "FY2088", "first");
+
+    var response = getData(financeDataEndpoint, TENANT_HEADER);
     var body = response.getBody().as(FyFinanceDataCollection.class);
     var actualFyFinanceData = body.getFyFinanceData().get(0);
 
-    assertEquals(FISCAL_YEAR_ID, actualFyFinanceData.getFiscalYearId());
+    assertEquals(fiscalYearId, actualFyFinanceData.getFiscalYearId());
     assertEquals(acqUnitId, actualFyFinanceData.getFundAcqUnitIds().get(0));
     assertEquals(acqUnitId, actualFyFinanceData.getBudgetAcqUnitIds().get(0));
   }
@@ -103,9 +108,11 @@ public class FinanceDataApiTest extends TestBase {
     var expectedTags = List.of("New tag");
     var expectedBudgetStatus = "Inactive";
     var expectedNumber = 200.0;
+    var financeDataEndpoint = String.format(FINANCE_DATA_ACQ_ENDPOINT_FORMAT, FINANCE_DATA_ENDPOINT, fiscalYearId, acqUnitId, acqUnitId);
+
     createMockData(fiscalYearId, acqUnitId, "FY2099", "second");
 
-    var response = getData(FINANCE_DATA_ENDPOINT + "?query=(fiscalYearId==" + fiscalYearId + ")", TENANT_HEADER);
+    var response = getData(financeDataEndpoint, TENANT_HEADER);
     var body = response.getBody().as(FyFinanceDataCollection.class);
     var fyFinanceData = body.getFyFinanceData().get(0);
 
@@ -116,14 +123,16 @@ public class FinanceDataApiTest extends TestBase {
     fyFinanceData.setBudgetAllowableExpenditure(expectedNumber);
     fyFinanceData.setBudgetAllowableEncumbrance(expectedNumber);
 
-    var updatedCollection = new FyFinanceDataCollection().withFyFinanceData(List.of(fyFinanceData)).withUpdateType(FyFinanceDataCollection.UpdateType.COMMIT).withTotalRecords(1);
+    var updatedCollection = new FyFinanceDataCollection()
+      .withFyFinanceData(List.of(fyFinanceData))
+      .withUpdateType(FyFinanceDataCollection.UpdateType.COMMIT).withTotalRecords(1);
 
     // Update finance data as a bulk
     var updateResponse = putData(FINANCE_DATA_ENDPOINT, JsonObject.mapFrom(updatedCollection).encodePrettily(), TENANT_HEADER);
     assertEquals(204, updateResponse.getStatusCode());
 
     // Get updated result
-    var updatedResponse = getData(FINANCE_DATA_ENDPOINT + "?query=(fiscalYearId==" + fiscalYearId + ")", TENANT_HEADER);
+    var updatedResponse = getData(financeDataEndpoint, TENANT_HEADER);
     var updatedBody = updatedResponse.getBody().as(FyFinanceDataCollection.class);
     var updatedFyFinanceData = updatedBody.getFyFinanceData().get(0);
 
@@ -139,19 +148,24 @@ public class FinanceDataApiTest extends TestBase {
     var budgetId = UUID.randomUUID().toString();
 
     var fiscalYear = new JsonObject(getFile(FISCAL_YEAR.getPathToSampleFile())).mapTo(FiscalYear.class)
-      .withId(fiscalYearId).withCode(code);
+      .withId(fiscalYearId).withCode(code).withName("TEST");
     createEntity(FISCAL_YEAR.getEndpoint(), fiscalYear, TENANT_HEADER);
 
-    var ledger = new JsonObject(getFile(LEDGER.getPathToSampleFile())).mapTo(Ledger.class).withId(ledgerId)
-      .withCode(code).withName(name).withFiscalYearOneId(fiscalYearId);
+    var ledger = new Ledger()
+      .withId(ledgerId)
+      .withCode(code).withName(name)
+      .withFiscalYearOneId(fiscalYearId)
+      .withLedgerStatus(Ledger.LedgerStatus.ACTIVE);
     createEntity(LEDGER.getEndpoint(), ledger, TENANT_HEADER);
 
-    var fund = new JsonObject(getFile(FUND.getPathToSampleFile())).mapTo(Fund.class)
-      .withId(fundId).withCode(code).withName(name).withLedgerId(ledgerId)
+    var fund = new Fund()
+      .withId(fundId).withCode(code).withName(name)
+      .withLedgerId(ledgerId)
+      .withFundStatus(Fund.FundStatus.ACTIVE)
       .withFundTypeId(null).withAcqUnitIds(List.of(acqUnitId));
     createEntity(FUND.getEndpoint(), fund, TENANT_HEADER);
 
-    var budget = new JsonObject(getFile(BUDGET.getPathToSampleFile())).mapTo(Budget.class)
+    var budget = new Budget()
       .withId(budgetId).withName(name)
       .withBudgetStatus(Budget.BudgetStatus.ACTIVE)
       .withFiscalYearId(fiscalYearId)
