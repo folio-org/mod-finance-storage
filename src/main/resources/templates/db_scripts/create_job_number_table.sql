26f1ecc004e61f13988692c7c409362718a8824a
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2025-03-06 16:21:43 +0500

    [MODFIN-415] - Make job_number start from 1

--- src/main/resources/templates/db_scripts/create_job_number_table.sql
+++ src/main/resources/templates/db_scripts/create_job_number_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS ${myuniversity}_${mymodule}.job_number (
   type varchar(255) NOT NULL,
-  last_number bigint NOT NULL DEFAULT 1,
+  last_number bigint NOT NULL DEFAULT 0,
   PRIMARY KEY(type)
 );
 
 INSERT INTO ${myuniversity}_${mymodule}.job_number (type, last_number)
-VALUES ('FundUpdateLogs', 1)
+VALUES ('FundUpdateLogs', 0)
 ON CONFLICT (type) DO NOTHING;
