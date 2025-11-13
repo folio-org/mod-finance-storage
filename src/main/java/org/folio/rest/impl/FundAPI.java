3d4e8a62c5989d5963fe8e98e0640280dd9d404a
Saba-Zedginidze-EPAM <148070844+Saba-Zedginidze-EPAM@users.noreply.github.com>
2025-11-13 19:20:00 +0400

    [MODFIN-443] Implement batch endpoints for fetching funds and budgets in mod-finance-storage (#503)
    * Add new API definitions
    
    * Implement batch endpoints
    
    * Update acq-models
    
    * Update mod descriptor with new permissions
    
    * Add tests
    
    * Update acq-models pointer

--- src/main/java/org/folio/rest/impl/FundAPI.java
+++ src/main/java/org/folio/rest/impl/FundAPI.java
@@ -1,20 +1,18 @@
 package org.folio.rest.impl;
 
 import static io.vertx.core.Future.succeededFuture;
+import static org.folio.rest.jaxrs.resource.FinanceStorageFunds.PostFinanceStorageFundsBatchResponse.respond200WithApplicationJson;
 import static org.folio.rest.jaxrs.resource.FinanceStorageGroupFundFiscalYears.PutFinanceStorageGroupFundFiscalYearsByIdResponse.respond204;
 
 import javax.ws.rs.core.Response;
 import java.util.Map;
 
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Context;
-import io.vertx.core.Handler;
-import io.vertx.core.Vertx;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.rest.annotations.Validate;
 import org.folio.rest.core.model.RequestContext;
 import org.folio.rest.exception.HttpException;
+import org.folio.rest.jaxrs.model.BatchIdCollection;
 import org.folio.rest.jaxrs.model.Fund;
 import org.folio.rest.jaxrs.model.FundCollection;
 import org.folio.rest.jaxrs.resource.FinanceStorageFunds;
@@ -23,6 +21,11 @@ import org.folio.rest.persist.PgUtil;
 import org.folio.service.fund.FundService;
 import org.folio.spring.SpringContextUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
 
 public class FundAPI implements FinanceStorageFunds {
 
@@ -51,6 +54,19 @@ public class FundAPI implements FinanceStorageFunds {
   }
 
   @Override
+  public void postFinanceStorageFundsBatch(BatchIdCollection entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    new RequestContext(vertxContext, okapiHeaders).toDBClient()
+      .withConn(conn -> fundService.getFundsByIds(entity.getIds(), conn)
+        .map(funds -> new FundCollection().withFunds(funds).withTotalRecords(funds.size()))
+        .onSuccess(funds -> asyncResultHandler.handle(succeededFuture(respond200WithApplicationJson(funds))))
+        .onFailure(throwable -> {
+          HttpException cause = (HttpException) throwable;
+          logger.error("Failed to get funds by ids {}", entity.getIds(), cause);
+          HelperUtils.replyWithErrorResponse(asyncResultHandler, cause);
+        }));
+  }
+
+  @Override
   @Validate
   public void getFinanceStorageFundsById(String id, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     PgUtil.getById(FUND_TABLE, Fund.class, id, okapiHeaders, vertxContext, GetFinanceStorageFundsByIdResponse.class, asyncResultHandler);
@@ -69,12 +85,12 @@ public class FundAPI implements FinanceStorageFunds {
     fund.setId(id);
     vertxContext.runOnContext(event ->
       fundService.updateFund(fund, new RequestContext(vertxContext, okapiHeaders))
-      .onSuccess(result -> asyncResultHandler.handle(succeededFuture(respond204())))
-      .onFailure(throwable -> {
-        HttpException cause = (HttpException) throwable;
-        logger.error("Failed to update the finance storage fund with Id {}", fund.getId(), cause);
-        HelperUtils.replyWithErrorResponse(asyncResultHandler, cause);
-      })
+        .onSuccess(result -> asyncResultHandler.handle(succeededFuture(respond204())))
+        .onFailure(throwable -> {
+          HttpException cause = (HttpException) throwable;
+          logger.error("Failed to update the finance storage fund with Id {}", fund.getId(), cause);
+          HelperUtils.replyWithErrorResponse(asyncResultHandler, cause);
+        })
     );
   }
 }
